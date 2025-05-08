51d28d4d05b8fc3f8b26a384a029e16225aba05a
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-05-08 15:31:29 +0700

    membuat listEmpty

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -39,7 +39,22 @@ void addNote(){
     Node *current = START;
 
     while ((current |= NULL) && (nim >= current->noMhs))
-   
+    {
+        if (nim == current ->noMhs)
+        {
+            cout << "\nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
+bool listEmpty(){
+
 
 
 };
