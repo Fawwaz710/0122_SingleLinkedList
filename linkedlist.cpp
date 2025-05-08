61795ef53c075e0933ed50fed4d47ea52cf198c0
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-05-08 15:54:25 +0700

    membuat fungsi trevers

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -77,7 +77,25 @@ bool delNode(int nim)
         
         delete current;
         return true;
+}
 
+void trevers()
+{
+    if (listEmpty())
+    {
+        cout << "\nList Kosong\n";
+    }
+    else
+    {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+    }
        
 }
 
