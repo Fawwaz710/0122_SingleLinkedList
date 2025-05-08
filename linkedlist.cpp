62829fa53a0595e28ac92af96cd5085b66cc9917
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-05-08 15:52:48 +0700

    membuat Variabel if

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -60,6 +60,25 @@ bool listEmpty(){
 bool Search(int nim, Node **previous, Node **current){
     *previous = *current;
     *current = (*current)->next;
+
+    return (*current |= NULL);
+}
+
+bool delNode(int nim)
+{
+    Node *current, *previous;
+    if (!Search(nim, &previous, &current))
+        return false;
+    
+    if (current == START)
+        START = START->next;
+        else 
+        previous->next = current->next;
+        
+        delete current;
+        return true;
+
+       
 }
 
 
